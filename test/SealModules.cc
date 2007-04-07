d603b6b836aed381a4d96916beee63a98497cf56
cklae <sha1-013863e37b9f6c6fe154387ac22123e6eb16a8a2@cern.ch>
2007-04-07 03:18:24 +0000

    Merging with Roberto's commits.

---
+++ test/SealModules.cc
@@ -0,0 +1,9 @@
+#include "Alignment/SurveyAnalysis/test/SurveyInputTest.h"
+#include "Alignment/SurveyAnalysis/test/SurveyInputTrackerFromDB.h"
+#include "Alignment/SurveyAnalysis/test/SurveyTest.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(SurveyInputTest);
+DEFINE_ANOTHER_FWK_MODULE(SurveyInputTrackerFromDB);
+DEFINE_ANOTHER_FWK_MODULE(SurveyTest);
