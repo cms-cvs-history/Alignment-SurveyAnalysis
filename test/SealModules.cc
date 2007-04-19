296b17f8041d343e0af8963d4e059693923b6803
cmsbuild <>
2007-04-19 22:50:18 +0000

    This commit was manufactured by cvs2git to create tag 'Alignment- SurveyAnalysis-V00-01-00-00'.
    Sprout from master 2007-04-09 01:16:14 UTC cklae <sha1-013863e37b9f6c6fe154387ac22123e6eb16a8a2@cern.ch> 'Use meaningful names.'
    Cherrypick from master 2007-04-19 22:50:17 UTC cklae <sha1-013863e37b9f6c6fe154387ac22123e6eb16a8a2@cern.ch> 'Make a version for Nhan to test.':
        src/SealModules.cc
    Delete:
        data/GetTibSurvey/GetTibSurvey.pl
        doc/html/index.html
        doc/html/overview.html
        interface/SurveyAlignment.h
        interface/SurveyAlignmentAlgorithm.h
        interface/SurveyAlignmentPoints.h
        interface/SurveyAlignmentSensor.h
        interface/SurveyDataConverter.h
        interface/SurveyDataReader.h
        interface/SurveyOutput.h
        interface/SurveyParameters.h
        src/SurveyAlignment.cc
        src/SurveyAlignmentAlgorithm.cc
        src/SurveyAlignmentPoints.cc
        src/SurveyAlignmentSensor.cc
        src/SurveyDataConverter.cc
        src/SurveyDataReader.cc
        src/SurveyOutput.cc
        src/SurveyParameters.cc
        test/AlignableTest.cc
        test/AlignableTest.h
        test/BuildFile
        test/PlotDiffs.C
        test/PlotDiffsAll.C
        test/PlotDiffsNotOk.C
        test/PlotDiffsOk.C
        test/SealModules.cc
        test/SurveyInputTest.cc
        test/SurveyInputTest.h
        test/SurveyTest.cc
        test/SurveyTest.h
        test/TestConverter.cpp
        test/TestIdealGeometry.cpp
        test/TestIdealGeometry2.cpp
        test/align.cfg
        test/alignment.cff
        test/detector.cfi
        test/run-converter.cfg
        test/survey.cfg
        test/test-analyze-ideal.cfg
        test/test-analyze.cfg

--- test/SealModules.cc
+++
@@ -1,9 +0,0 @@
-#include "Alignment/SurveyAnalysis/test/SurveyInputTest.h"
-#include "Alignment/SurveyAnalysis/test/SurveyInputTrackerFromDB.h"
-#include "Alignment/SurveyAnalysis/test/SurveyTest.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(SurveyInputTest);
-DEFINE_ANOTHER_FWK_MODULE(SurveyInputTrackerFromDB);
-DEFINE_ANOTHER_FWK_MODULE(SurveyTest);
