dc13cac50e99aa2943d9a3cd4c27efe4462eb6b4
Nhan Viet Tran <sha1-8a19d334a4648fb26ba9e7879a577ce46f3df51a@cern.ch>
2007-05-08 22:36:47 +0000

    Added new text file uploader, move module from test to plugins

--- plugins/SurveyInputTrackerFromDB.h
+++ plugins/SurveyInputTrackerFromDB.h
@@ -5,33 +5,38 @@
  *
  *  Class to read ideal tracker from DB.
  *
- *  $Date: 2007/01/17 $
- *  $Revision: 1 $
+ *  $Date: 2007/04/07 01:58:49 $
+ *  $Revision: 1.1 $
  *  \author Chung Khim Lae
  */
-
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "Alignment/SurveyAnalysis/interface/SurveyInputBase.h"
+#include "Alignment/SurveyAnalysis/interface/SurveyInputTextReader.h"
 
 class SurveyInputTrackerFromDB:
   public SurveyInputBase
 {
-  public:
-
+public:
+	
   SurveyInputTrackerFromDB(
-			   const edm::ParameterSet&
-			   ) {}
-
+		const edm::ParameterSet&
+		);
+	
   /// Read ideal tracker geometry from DB
   virtual void beginJob(
-			const edm::EventSetup&
-			);
-
-  private:
-
+		const edm::EventSetup&
+		);
+	
+private:
+	
+	SurveyInputTextReader::MapType uIdMap;
+	//edm::ParameterSet theParameterSet;	
+	std::string textFileName;
+	
   /// Add survey info to an alignable
   void addSurveyInfo(
-		     Alignable*
-		     );
+		Alignable*
+		);
 };
 
 #endif
