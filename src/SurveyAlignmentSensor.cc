d9824444bf489fe6cb74a0edb591ca9018c841f1
cmsbuild <>
2007-11-16 10:56:07 +0000

    This commit was manufactured by cvs2git to create tag 'Alignment- SurveyAnalysis-CMSSW_1_8_0_pre2'.
    Sprout from master 2007-11-16 10:56:06 UTC Frederic Ronga <sha1-6292746f5aa3899cb618bde7cfd1e4b3e53eb4bb@cern.ch> 'Adapted to split in CondFormats/DataRecord'
    Cherrypick from master 2007-10-08 16:38:04 UTC cklae <sha1-013863e37b9f6c6fe154387ac22123e6eb16a8a2@cern.ch> 'Use align namespace; TrackerAligmentId change; clean up headers.':
        plugins/SurveyAlignmentAlgorithm.cc
        src/SurveyAlignmentPoints.cc
        src/SurveyAlignmentSensor.cc
        src/SurveyOutput.cc

--- src/SurveyAlignmentSensor.cc
+++ src/SurveyAlignmentSensor.cc
@@ -25,8 +25,6 @@ void SurveyAlignmentSensor::findAlignPars(bool bias)
     {
       SurveyResidual res(*ali, theLevels[l], bias);
 
-      if ( !res.valid() ) continue;
-
       AlgebraicSymMatrix invCov = res.inverseCovariance();
 
       par += invCov * res.sensorResidual();
